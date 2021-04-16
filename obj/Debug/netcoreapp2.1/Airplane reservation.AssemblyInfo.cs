54d77cab0a399caf84f1124d19b947cae6dcae49
unknown <mani91g@gmail.com>
2021-04-16 17:54:17 +0530

    MInor changes related to deployment

--- obj/Debug/netcoreapp2.1/Airplane reservation.AssemblyInfo.cs
+++ obj/Debug/netcoreapp2.1/Airplane reservation.AssemblyInfo.cs
@@ -7,7 +7,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Airplane reservation")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Name")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
